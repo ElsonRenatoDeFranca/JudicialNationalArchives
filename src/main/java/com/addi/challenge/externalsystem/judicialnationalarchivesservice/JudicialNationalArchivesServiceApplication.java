1d6e6ee5f1a370d33abc91cf0f6eb63d20168157
ElsonRenatoDeFranca <elson.renato.de.franca@gmail.com>
2022-03-29 00:17:24 -0300

    feature-001 Initial commit

---
+++ src/main/java/com/addi/challenge/externalsystem/judicialnationalarchivesservice/JudicialNationalArchivesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.addi.challenge.externalsystem.judicialnationalarchivesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudicialNationalArchivesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudicialNationalArchivesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addi/challenge/externalsystem/judicialnationalarchivesservice/JudicialNationalArchivesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addi.challenge.externalsystem.judicialnationalarchivesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudicialNationalArchivesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
