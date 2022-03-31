d91ee443080ca57868365cc5431b640ae5caf93b
ElsonRenatoDeFranca <elson.renato.de.franca@gmail.com>
2022-03-31 12:57:39 -0300

    Feature-0002 - Added unit tests

--- src/test/java/com/addi/challenge/externalsystem/judicialnationalarchivessystem/service/JudicialNationalArchivesSystemServiceImplTest.java
+++ src/test/java/com/addi/challenge/externalsystem/judicialnationalarchivessystem/service/JudicialNationalArchivesSystemServiceImplTest.java
@@ -2,35 +2,31 @@ package com.addi.challenge.externalsystem.judicialnationalarchivessystem.service
 
 import com.addi.challenge.externalsystem.judicialnationalarchivessystem.entity.Person;
 import com.addi.challenge.externalsystem.judicialnationalarchivessystem.repository.JudicialNationalArchivesSystemRepository;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @ExtendWith(MockitoExtension.class)
 class JudicialNationalArchivesSystemServiceImplTest {
-
 
     @Mock
     private JudicialNationalArchivesSystemRepository repository;
 
     @InjectMocks
     private JudicialNationalArchivesSystemServiceImpl judicialNationalArchivesSystemService;
+
+    private static final String PERSON_NOT_FOUND_EXCEPTION = "Person not found";
 
     @Test
     public void shouldReturnNotNullListWhenFindAllIsCalled(){
@@ -50,18 +46,40 @@ class JudicialNationalArchivesSystemServiceImplTest {
 
     @Test
     public void shouldReturnNotNullWhenFindByIdIsCalled(){
-        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(createPersonMock()));
-        Person actual = this.judicialNationalArchivesSystemService.findPersonById(1L);
+        Person expectedPerson = createPersonMock();
 
-        assertThat(actual).isNotNull();
-        assertThat(actual).isEqualTo(createPersonMock());
+        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(expectedPerson));
+
+        Person actualPerson = this.judicialNationalArchivesSystemService.findPersonById(expectedPerson.getId());
+
+        assertThat(actualPerson).isNotNull();
+        assertThat(actualPerson).isEqualTo(expectedPerson);
+
+    }
+
+    @Test
+    public void shouldThrowAPersonNotFoundExceptionWhenFindByIdIsCalledWithUnknownId(){
+        Person expectedPerson = createPersonMock();
+
+        Mockito.when(repository.findById(Mockito.anyLong())).thenThrow(new RuntimeException(PERSON_NOT_FOUND_EXCEPTION));
+
+        Throwable exception = assertThrows(RuntimeException.class,
+                () -> this.judicialNationalArchivesSystemService.findPersonById(expectedPerson.getId()));
+
+        assertThat(PERSON_NOT_FOUND_EXCEPTION).isEqualTo(exception.getMessage());
     }
 
     private List<Person> createPeopleMockList(){
-        return Arrays.asList(createPersonMock(),createPersonMock());
+        return Arrays.asList(createPersonMock(), createPersonMock());
     }
 
     private Person createPersonMock(){
-        return Person.builder().nationalIdentificationNumber("1").firstName("John").lastName("McCain").birthDate("22/01/1978").build();
+        return Person.builder()
+                .id(1L)
+                .nationalIdentificationNumber("1")
+                .firstName("Lebron")
+                .lastName("James")
+                .birthDate("22/01/1978")
+                .build();
     }
 }
